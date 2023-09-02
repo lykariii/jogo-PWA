5f5754aeb46fcb89bb473960ec93b14a3d59e1a1
any <karendecleva3@gmail.com>
2023-09-02 17:00:31 -0400

    js funcionando

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+window.onload = () => {
+    "use strict";
+    if("serviceWorker" in navigator){
+        navigator.serviceWorker.register("./sw.js");
+    }
+};
+
+localStorage.setItem("jogador", "X")
+localStorage.setItem("jogo", "")
+
 function marcar(id){
-    document.getElementById(id).innerHTML = "X"
+    var jogador = localStorage.getItem("jogador")
+    var jogo = localStorage.getItem("jogo")
+    var botao = document.getElementById(id);
+    if (!botao.innerHTML){
+        botao.innerHTML = jogador;
+
+    if (jogador == "X") localStorage.setItem("jogador", "O");
+    else localStorage.setItem("jogador", "X");
+    jogo = jogo.substring(0, Number(id) - 1) + jogador + jogo .substring(Number(id));
+    localStorage.setItem("jogo", jogo);
+    }
+    
 }
